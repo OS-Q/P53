f1834c7cc72b2666138d783f39f2ccdebfa196c6
qitas <qitas@hotmail.com>
2019-10-13 16:46:28 +0800

    fix

--- builder/frameworks/mbed.py
+++ builder/frameworks/mbed.py
@@ -1,17 +1,3 @@
-"""
-mbed
-
-The mbed framework The mbed SDK has been designed to provide enough
-hardware abstraction to be intuitive and concise, yet powerful enough to
-build complex projects. It is built on the low-level ARM CMSIS APIs,
-allowing you to code down to the metal if needed. In addition to RTOS,
-USB and Networking libraries, a cookbook of hundreds of reusable
-peripheral and module libraries have been built on top of the SDK by
-the mbed Developer Community.
-
-http://mbed.org/
-"""
-
 from os.path import join
 
 from SCons.Script import Import, SConscript
