ce38c8bef7970ac5e86dd3071f220c13c4889c27
qitas <qitas@qitas.cn>
2021-02-22 12:00:12 +0800

    fix

--- builder/frameworks/mbed.py
+++ builder/frameworks/mbed.py
@@ -4,7 +4,6 @@ from SCons.Script import Import, SConscript
 
 Import("env")
 
-# https://github.com/platformio/builder-framework-mbed.git
 SConscript(
-    join(env.PioPlatform().get_package_dir("framework-mbed"), "platformio",
+    join(env.PioPlatform().get_package_dir("mbed"), "platformio",
         "platformio-build.py"))
